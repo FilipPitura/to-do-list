a522070a3b7694498cbc2b3e553aaf01a83b6f28
Filip Pitura <filippitura123@gmail.com>
2023-01-08 23:33:09 +0100

    added css and adding tasks

--- js/script.js
+++ js/script.js
@@ -15,7 +15,8 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
+            <li class="list__item${task.done ? " list__item--done" : ""}"
+            >
             ${task.content}
             </li>
             `
@@ -24,8 +25,33 @@
         document.querySelector(".js-tasks").innerHTML = htmlString
     };
 
+    const addNewtask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+ 
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return; 
+        }
+
+        addNewtask(newTaskContent);
+        };
+
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("sumbit", onFormSubmit);
     };
 
     init();
